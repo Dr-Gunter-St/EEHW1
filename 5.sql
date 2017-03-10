3fbc1ac4b6c11bcb7ddb6e5adb8c85cfe88c99ee
Dr-Gunter-St <vladleonovv@gmail.com>
2017-03-11 00:08:43 +0200

    Almost finished(1)

--- 5.sql
+++ 5.sql
@@ -1,14 +1,14 @@
-  
 
-SELECT * FROM (
+SELECT company_name, customer_name FROM (
 SELECT
      companies.name AS company_name,
      min(projects.cost)  AS cost,
      projects.name  AS project_name,
-     customers.name AS customer_name
+     customers.name AS customer_name,
+     projects.id AS project_id
    FROM companies
      JOIN projects ON companies.id = projects.company_id
-     JOIN customers ON projects.customer_id = customers.id GROUP BY customer_name) a
+     JOIN customers ON projects.customer_id = customers.id GROUP BY customer_name ) a
 GROUP BY company_name
 
   
